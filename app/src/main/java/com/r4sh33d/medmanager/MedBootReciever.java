60c3c08d2e70934c873523a7323963bcb22471f5
Rasheed Sulayman <sulaymanrasheed@gmail.com>
2018-04-04 21:55:05 +0100

    Add boot reciever class to restart scheduled alarm

---
+++ app/src/main/java/com/r4sh33d/medmanager/MedBootReciever.java
@@ -0,0 +1,12 @@
+package com.r4sh33d.medmanager;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class MedBootReciever extends BroadcastReceiver{
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
